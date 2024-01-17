590d640f972792c84ac3cee89486c1071c47669a
o-master-o <kasian.m.w@gmail.com>
2024-01-17 22:10:14 +0100

    fix help

--- start.py
+++ start.py
@@ -1,17 +1,48 @@
+#!/home/yoda/work/python_projects/NumFun/venv/bin/python
 import typer
+from rich.console import Console
+from rich.panel import Panel
+
+console = Console()
+
+
+HEADER = """[blue] \n
+[yellow]
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                                                   ┃
+┃   ███╗   ██╗██╗   ██╗███╗   ███╗    ███████╗██╗   ██╗███╗   ██╗   ┃
+┃   ████╗  ██║██║   ██║████╗ ████║    ██╔════╝██║   ██║████╗  ██║   ┃
+┃   ██╔██╗ ██║██║   ██║██╔████╔██║    █████╗  ██║   ██║██╔██╗ ██║   ┃
+┃   ██║╚██╗██║██║   ██║██║╚██╔╝██║    ██╔══╝  ██║   ██║██║╚██╗██║   ┃
+┃   ██║ ╚████║╚██████╔╝██║ ╚═╝ ██║    ██║     ╚██████╔╝██║ ╚████║   ┃
+┃   ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝    ╚═╝      ╚═════╝ ╚═╝  ╚═══╝   ┃
+┃                                                                   ┃
+┃   ═══════════ Welcome to NumFun! Let's Enjoy Math! ════════════   ┃
+┃                                                                   ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
 
 
 def game_app():
 
-    app = typer.Typer()
+    app = typer.Typer(
+        no_args_is_help=True,
+        rich_markup_mode='rich',
+        name='num-fun',
+        help=HEADER,
+    )
 
-    @app.command()
+    @app.command(help='this is game x-pedition')
     def x_pedition():
         print(f"Hello x_pedition")
 
     @app.command()
-    def digit_detective():
+    def digit_detective(help='this is game digit-detectiven'):
         print(f"Hello digit_detective")
+
+    @app.command()
+    def calculator(help='this is game calculator'):
+        print(f"Hello calculator")
 
     return app
 
