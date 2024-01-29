4a1895d9be3a769b18e634d340461a3567c0e177
o-master-o <kasian.m.w@gmail.com>
2024-01-29 11:42:45 +0100

    otimize imports

--- games/calculator.py
+++ games/calculator.py
@@ -1,8 +1,8 @@
-def calcul():
+def main():
     while True:
         max_limit = input('Enter expression\n')
         print(eval(max_limit))
 
 
 if __name__ == "__main__":
-    calcul()
+    main()
