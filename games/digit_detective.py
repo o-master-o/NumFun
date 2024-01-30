bb45d5c85ce9804554d6e2d1b173d8f7550be7c4
o-master-o <kasian.m.w@gmail.com>
2024-01-30 22:44:11 +0100

    refactor Digit-detective

--- games/digit_detective.py
+++ games/digit_detective.py
@@ -1,36 +1,54 @@
 import random
 
 from games.base import Game
-from ui.cli_ui import CliUI
 
 
-class NumberGuessingGame:
-    def __init__(self, min_num, max_num):
-        self.min_num = min_num
-        self.max_num = max_num
-        self.target_num = random.randint(min_num, max_num)
+class DigitDetective(Game):
+    NAME = "Digit-detective"
 
-    def check_guess(self, guess):
+    def __init__(self, ui):
+        super().__init__(ui)
+        self.min_num = 1
+        self.max_num = 100
+        self.target_num = None
+
+    def start(self):
+        self.max_num = self._ask_user_for_max_number()
+        self.ui.display_message(f"[green]Now Guessing Game! Guess a number between {self.min_num} and {self.max_num}.[/]")
+
+        self.target_num = random.randint(self.min_num, self.max_num)
+
+        while True:
+            guess = int(input("Enter your guess: "))
+            result = self._check_guess(guess)
+            if result == "correct":
+                print("Congratulations! You've guessed the number!")
+                break
+            else:
+                print(f"Try guessing {result}.")
+
+    def _ask_user_for_max_number(self):
+        while True:
+            answer = self.ui.ask_question("[yellow]Please provide maximal number bigger than 1 you want to guess, or press Enter to use 100\n"
+                                          "  [dim]Your answer: [/]")
+            max_number = self._evaluate_max_value(answer)
+            if max_number:
+                return max_number
+
+    def _evaluate_max_value(self, value):
+        try:
+            max_int_num = int(value)
+            if max_int_num <= 1:
+                raise ValueError
+            return max_int_num
+        except ValueError:
+            self.ui.display_message(f"[bold red] Value should be integer number, and bigger than 1")
+
+
+    def _check_guess(self, guess):
         if guess < self.target_num:
             return "higher"
         elif guess > self.target_num:
             return "lower"
         else:
             return "correct"
-
-
-class DigitDetective(Game):
-    NAME = "Digit-detective"
-
-    def start(self):
-        game = NumberGuessingGame(1, 100)
-        print(f"Welcome to the Number Guessing Game! Guess a number between {game.min_num} and {game.max_num}.")
-
-        while True:
-            guess = int(input("Enter your guess: "))
-            result = game.check_guess(guess)
-            if result == "correct":
-                print("Congratulations! You've guessed the number!")
-                break
-            else:
-                print(f"Try guessing {result}.")
