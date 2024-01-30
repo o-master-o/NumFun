c536cd18df17e4bf3bb446fe8a6359285be4480c
o-master-o <kasian.m.w@gmail.com>
2024-01-30 20:32:34 +0100

    format game introduction text

--- ui/cli_ui.py
+++ ui/cli_ui.py
@@ -1,5 +1,6 @@
 import os
 from rich.panel import Panel
+from rich.text import Text
 from rich.console import Console
 import emoji
 
@@ -9,6 +10,7 @@ from ui.base import UI
 class CliUI(UI):
     CONGRATULATIONS = f"{emoji.emojize(':smiling_cat_with_heart-eyes:')} Congratulations!"
     COMMISERATIONS = f"{emoji.emojize(':crying_cat:')} Try again!"
+    LIGHT_YELLOW = "#ffffbf"
 
     def __init__(self):
         self._console = Console()
@@ -18,16 +20,18 @@ class CliUI(UI):
 
     def display_game_introduction(self, game_info):
         introduction = (
-            f"{game_info['header']}"
-            f"\n  [bold]Description[not bold]\n"
-            f"  {game_info['description']}\n"
-            f"  [bold]How to Play[not bold]\n"
-            f"  {game_info['instruction']}\n"
-            f"  [bold]Control[not bold]\n"
-            f"  {game_info['control']}"
+            f"[yellow]{game_info['header']}[/]\n"
+            f"[bold yellow]Description[/]\n"
+            f"[{self.LIGHT_YELLOW}]{game_info['description']}[/]\n"
+            f"\n"
+            f"[bold yellow]How to Play[/]\n"
+            f"[{self.LIGHT_YELLOW}]{game_info['instruction']}[/]\n"
+            f"\n"
+            f"[bold yellow]Control[/]\n"
+            f"[{self.LIGHT_YELLOW}]{game_info['control']}[/]"
         )
 
-        self._console.print(introduction)
+        self._console.print(Panel(introduction, expand=False, border_style="yellow"))
 
     def display_message(self, message):
         self._console.print(f"  {message}")
