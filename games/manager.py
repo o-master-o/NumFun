c9f1eac891f846e6f7c9fd18a940bf1a3b71c999
o-master-o <kasian.m.w@gmail.com>
2024-01-30 11:20:22 +0100

    add loop and try to GameManager

--- games/manager.py
+++ games/manager.py
@@ -1,23 +1,43 @@
 from typing import Type
 from games.base import Game
 from prompt_toolkit import prompt
+
+from games.calculator import Calculator
+from games.digit_detective import DigitDetective
+from games.x_pedition import Xpedition
 from ui.base import UI
 from prompt_toolkit.completion import WordCompleter
+
+
+games_map = {
+    "x-pedition": Xpedition,
+    "digit-detective": DigitDetective,
+    "calculator": Calculator
+}
 
 
 class GameManager:
     def __init__(self, ui: Type[UI]):
         self.ui = ui
 
-    def choose_game(self, games_list: dict[str, Type[Game]]) -> None:
+    def start(self):
+        while True:
+            self._choose_and_play_game()
 
-        game_completer = WordCompleter(list(games_list.keys()))
+    def _choose_and_play_game(self):
+        try:
+            self._play_game(self._choose_game(games_map))
+        except KeyboardInterrupt:
+            print('Exit game')
+
+    def _choose_game(self, games_map: dict[str, Type[Game]]) -> Type[Game]:
+        game_completer = WordCompleter(list(games_map.keys()))
         selected_game = prompt("Choose a game: ", completer=game_completer)
-
-        if selected_game in games_list:
-            game_instance = games_list[selected_game](ui=self.ui)
-            game_instance.start()
-        else:
+        game = games_map.get(selected_game)
+        if game is None:
             print("Game not found.")
+        else:
+            return game
 
-
+    def _play_game(self, game):
+        game(ui=self.ui).start()
