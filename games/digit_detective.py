8c4f25d31a2f0d436cce184dc6cf0f97f49a5d9c
o-master-o <kasian.m.w@gmail.com>
2024-02-01 16:49:55 +0100

    improve Digit-detective prints and logic

--- games/digit_detective.py
+++ games/digit_detective.py
@@ -14,20 +14,29 @@ class DigitDetective(Game):
         self.target_num = None
 
     def start(self):
-        self.max_num = self._ask_user_for_max_number()
-        self.ui.display_message(f"[green]Maximal value has been set to {self.max_num}")
-        self.ui.display_message(f"[green]Now Guessing Game! Guess a number between {self.min_num} and {self.max_num}.[/]")
-
-        self.target_num = random.randint(self.min_num, self.max_num)
-
         while True:
-            guess = int(input("Enter your guess: "))
-            result = self._check_guess(guess)
+            self._play()
+
+    def _play(self):
+        self._generate_number()
+        self._gues_number()
+
+    def _gues_number(self):
+        self.ui.display_message(f"[green]Now Guessing Game! Guess a number between {self.min_num} and {self.max_num}[/]")
+        while True:
+            user_guess = self._ask_user_to_guess_number()
+            result = self._check_guess(user_guess)
             if result == "correct":
-                print("Congratulations! You've guessed the number!")
+                self.ui.display_message(f"\n[blink]🌟[not blink] [bold yellow]Congratulations[not bold] [blink]🌟[not blink] "
+                                        f"You've guessed the number!\n")
                 break
             else:
-                print(f"Try guessing {result}.")
+                self.ui.display_message(f"[{self.ui.LIGHT_YELLOW}]Target number is {result}")
+
+    def _generate_number(self):
+        self.max_num = self._ask_user_for_max_number()
+        self.ui.display_message(f"[green]Maximal value has been set to {self.max_num}")
+        self.target_num = random.randint(self.min_num, self.max_num)
 
     def _ask_user_for_max_number(self):
         while True:
@@ -48,11 +57,20 @@ class DigitDetective(Game):
         except ValueError:
             self.ui.display_message(f"[bold red] Value should be integer number, and bigger than 1")
 
+    def _ask_user_to_guess_number(self):
+        while True:
+            answer = self.ui.ask_question(f"[{self.ui.LIGHT_YELLOW}]Enter your guess: ")
+            try:
+                return int(answer)
+            except ValueError:
+                self.ui.display_message(f"[{self.ui.LIGHT_YELLOW}]Incorrect input. Enter your guess: ")
+                continue
+
 
     def _check_guess(self, guess):
         if guess < self.target_num:
-            return "higher"
+            return "bigger ▲"
         elif guess > self.target_num:
-            return "lower"
+            return "smaller ▼"
         else:
             return "correct"
