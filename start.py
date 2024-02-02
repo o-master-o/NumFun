914cf7da5dcc1352a370f976f1d519ff388e5733
o-master-o <kasian.m.w@gmail.com>
2024-02-02 11:04:55 +0100

    move typer app to class NumFun

--- start.py
+++ start.py
@@ -9,41 +9,42 @@ from ui.cli_ui import CliUI
 from ui.gui import GUI
 from utils import update_git_repo
 
-REPO_PATH = Path(__file__).parent
-INTERFACES = {True: GUI, False: CliUI}
 
+class NumFun:
+    REPO_PATH = Path(__file__).parent
+    INTERFACES = {True: GUI, False: CliUI}
 
-def _format_text(text):
-    print(type(text))
-    print(text.split('\n'))
+    def _get_console_man(self):
+        info = get_game_info('num-fun')
+        header = self._format_header(info['header'])
+        return header
 
+    def _format_header(self, header):
+        lines = header.split('\n')
+        return '\n'.join([f"{lines[0]}\n"] + lines[1:])
 
-def get_console_man():
-    info = get_game_info('num-fun')
-    return (f"{info['header']}\n"
-            f"{info['description']}")
+    def game_app(self):
 
+        app = typer.Typer(
+            rich_markup_mode='rich',
+            name='num-fun',
+            help=self._get_console_man(),
+            context_settings={"help_option_names": ["-h", "--help"]}
+        )
 
-def game_app():
+        @app.callback(invoke_without_command=True)
+        def main(ctx: Context,
+                 gui_flag: bool = typer.Option(False, "--gui", "-g", help="Starts main game interface in graphical mode")):
+            if ctx.invoked_subcommand is None:
+                GameManager(self.INTERFACES[gui_flag]).start()
 
-    app = typer.Typer(
-        rich_markup_mode='rich',
-        name='num-fun',
-        help=get_console_man()
-    )
+        @app.command(name='update', short_help='Update application')
+        def update():
+            update_git_repo(self.REPO_PATH)
 
-    @app.callback(invoke_without_command=True)
-    def main(ctx: Context,
-             gui_flag: bool = typer.Option(False, "--gui", "-g", help="Starts main game interface in graphical mode")):
-        if ctx.invoked_subcommand is None:
-            GameManager(INTERFACES[gui_flag]).start()
-
-    @app.command(name='update', short_help='Update application')
-    def update():
-        update_git_repo(REPO_PATH)
-
-    return app
+        return app
 
 
 if __name__ == "__main__":
-    game_app()()
+    NumFun().game_app()()
+    # get_console_man()
