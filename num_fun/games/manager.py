c1e777c12ce382336269e4f1ca662e344166a451
o-master-o <kasian.m.w@gmail.com>
2024-02-16 21:18:11 +0100

    hide ui member in GameManager

--- num_fun/games/manager.py
+++ num_fun/games/manager.py
@@ -21,7 +21,7 @@ class GamesPocket:
 
 class GameManager:
     def __init__(self, ui, games_list):
-        self.ui = ui()
+        self._ui = ui()
         self._games_pocket = GamesPocket(games_list)
         self._selected_game = None
 
@@ -29,33 +29,31 @@ class GameManager:
         info = get_game_info('num-fun')
         play = True
         while play:
-            self.ui.reset_screen()
-            self.ui.display_message(info['header'])
+            self._ui.reset_screen()
+            self._ui.display_message(info['header'])
             play = self._choose_and_play_game()
+        self._exit_num_fun()
 
     def _choose_and_play_game(self):
         try:
-            selected_game_name = self.ui.ask_user_to_select_game(self._games_pocket.names)
+            selected_game_name = self._ui.ask_user_to_select_game(self._games_pocket.names)
             self._selected_game = self._games_pocket.get(selected_game_name)
             self._play_game()
             return True
         except KeyboardInterrupt:
-            self._exit_num_fun()
             return False
-        finally:
-            self._selected_game = None
 
     def _play_game(self):
-        self.ui.reset_screen()
+        self._ui.reset_screen()
         try:
-            game = self._selected_game(ui=self.ui)
+            game = self._selected_game(ui=self._ui)
             while True:
                 game.start()
         except KeyboardInterrupt:
-            self.ui.display_message(f'\n[yellow]Exit game [bold]{self._selected_game.NAME}[/]')
+            self._ui.display_message(f'\n[yellow]Exit game [bold]{self._selected_game.NAME}[/]')
             return
 
     def _exit_num_fun(self):
-        self.ui.reset_screen()
-        self.ui.display_message('\n[yellow] You left the game [b]NumFun[not b]. \n '
-                                'See you Later. 😉 Bye.. \n[/]')
+        self._ui.reset_screen()
+        self._ui.display_message('\n[yellow] You left the game [b]NumFun[not b]. \n '
+                                 'See you Later. 😉 Bye.. \n[/]')
